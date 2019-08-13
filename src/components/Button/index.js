2dd7749de9049dd8724da31d8fcc65f737de28ef
Roman84asas <roma.mereneanu@mail.ru>
2019-08-13 13:48:52 +0300

    Add AntD and test it

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -7,7 +7,7 @@ import './Buton.scss';
 
 const Button = props => {
     return (
-        <BasicButton className="button" type="dashed" {...props}/>
+        <BasicButton className="button" {...props}/>
     )
 };
 
