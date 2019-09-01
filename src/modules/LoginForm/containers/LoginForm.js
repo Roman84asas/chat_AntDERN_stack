4c6c963886de08db251454b8086e8dd4857a317a
RomanMereneanu <asasin84@mail.ru>
2019-09-01 16:53:30 +0300

    work with Redux and back

--- src/modules/LoginForm/containers/LoginForm.js
+++ src/modules/LoginForm/containers/LoginForm.js
@@ -16,10 +16,7 @@ export default withFormik({
         return errors;
     },
     handleSubmit: (values, { setSubmitting }) => {
-        setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-        }, 1000);
+
     },
     displayName: "LoginForm"
 })(LoginForm);
