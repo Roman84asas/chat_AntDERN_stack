9a687bd1f8ad08c314af827cfb0a6716e24e8e74
RomanMereneanu <asasin84@mail.ru>
2019-09-06 21:58:13 +0300

    work with frontend

--- src/components/ChatInput/index.js
+++ src/components/ChatInput/index.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { Button, Input } from "antd";
 import { UploadField } from "@navjobs/upload";
 import { Picker } from "emoji-mart";
+
+import { UploadFiles } from "../../components";
 
 import "./ChatInput.scss";
 
@@ -46,51 +48,56 @@ const ChatInput = props => {
 
 
   return (
-    <div className="chat-input">
-      <div className="chat-input__smile-btn">
+    <Fragment>
+        <div className="chat-input">
+            <div className="chat-input__smile-btn">
 
-        <div className="chat-input__emoji-picker">
-            {emojiPickerVisible && (
-               <Picker  onSelect={(emojiTag) => setEmojiToInputValue(emojiTag)} set="apple" />
-            )}
+                <div className="chat-input__emoji-picker">
+                    {emojiPickerVisible && (
+                        <Picker  onSelect={(emojiTag) => setEmojiToInputValue(emojiTag)} set="apple" />
+                    )}
+                </div>
+
+                <Button
+                    onClick={toggleEmojiPicker}
+                    type="link"
+                    shape="circle"
+                    icon="smile"
+                />
+            </div>
+
+            <TextArea
+                onChange={e => setValue(e.target.value)}
+                onKeyUp={handleSendMessage}
+                size="large"
+                placeholder="Введите текст сообщения…"
+                value={value}
+                autosize={{ minRows: 1, maxRows: 6 }}
+            />
+            <div className="chat-input__actions">
+                <UploadField
+                    onFiles={files => console.log(files)}
+                    containerProps={{
+                        className: "chat-input__actions-upload-btn"
+                    }}
+                    uploadProps={{
+                        accept: ".jpg,.jpeg,.png,.gif,.bmp",
+                        multiple: "multiple"
+                    }}
+                >
+                    <Button type="link" shape="circle" icon="camera" />
+                </UploadField>
+                {value ? (
+                    <Button type="link" shape="circle" icon="check-circle" />
+                ) : (
+                    <Button type="link" shape="circle" icon="audio" />
+                )}
+            </div>
         </div>
-
-        <Button
-          onClick={toggleEmojiPicker}
-          type="link"
-          shape="circle"
-          icon="smile"
-        />
-      </div>
-
-      <TextArea
-        onChange={e => setValue(e.target.value)}
-        onKeyUp={handleSendMessage}
-        size="large"
-        placeholder="Введите текст сообщения…"
-        value={value}
-        autosize={{ minRows: 1, maxRows: 6 }}
-      />
-      <div className="chat-input__actions">
-        <UploadField
-          onFiles={files => console.log(files)}
-          containerProps={{
-            className: "chat-input__actions-upload-btn"
-          }}
-          uploadProps={{
-            accept: ".jpg,.jpeg,.png,.gif,.bmp",
-            multiple: "multiple"
-          }}
-        >
-          <Button type="link" shape="circle" icon="camera" />
-        </UploadField>
-        {value ? (
-          <Button type="link" shape="circle" icon="check-circle" />
-        ) : (
-          <Button type="link" shape="circle" icon="audio" />
-        )}
-      </div>
-    </div>
+        <div>
+            <UploadFiles />
+        </div>
+    </Fragment>
   );
 };
 
---
+++ src/components/UploadFiles/index.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+
+import { Upload, Modal } from 'antd';
+
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+
+class UploadFiles extends React.Component {
+    state = {
+        previewVisible: false,
+        previewImage: '',
+        fileList: [
+            {
+                uid: '-1',
+                name: 'image.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            },
+            {
+                uid: '-2',
+                name: 'image.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            },
+            {
+                uid: '-3',
+                name: 'image.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            },
+            {
+                uid: '-4',
+                name: 'image.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            },
+            {
+                uid: '-5',
+                name: 'image.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            },
+        ],
+    };
+
+    handleCancel = () => this.setState({ previewVisible: false });
+
+    handlePreview = async file => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+
+        this.setState({
+            previewImage: file.url || file.preview,
+            previewVisible: true,
+        });
+    };
+
+    handleChange = ({ fileList }) => this.setState({ fileList });
+
+    render() {
+        const { previewVisible, previewImage, fileList } = this.state;
+
+        return (
+            <div className="clearfix">
+                <Upload
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={this.handlePreview}
+                    onChange={this.handleChange}
+                >
+                </Upload>
+                <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+            </div>
+        );
+    }
+}
+export default UploadFiles;
