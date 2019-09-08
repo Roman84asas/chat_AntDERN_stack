f5c3eb2617373ac6a26fbae920bc99e4e13b1ba5
RomanMereneanu <asasin84@mail.ru>
2019-09-08 18:13:00 +0300

    work with frontend

--- src/components/UploadFiles/index.jsx
+++ src/components/UploadFiles/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 import { Upload, Modal } from 'antd';
@@ -12,49 +12,42 @@ function getBase64(file) {
     });
 }
 
-class UploadFiles extends React.Component {
-    state = {
+const UploadFiles = () => {
+    const [state, setState] = useState({
         previewVisible: false,
-        previewImage: '',
-        fileList: [
+        previewImage: "",
+        fileList: []
+    });
 
-        ],
-    };
+    const handleCancel = () => setState({ ...state, previewVisible: false });
 
-    handleCancel = () => this.setState({ previewVisible: false });
-
-    handlePreview = async file => {
+    const handlePreview = async file => {
         if (!file.url && !file.preview) {
             file.preview = await getBase64(file.originFileObj);
         }
 
-        this.setState({
-            previewImage: file.url || file.preview,
-            previewVisible: true,
+        setState({ ...state,
+                    previewImage: file.url || file.preview,
+                    previewVisible: true,
         });
     };
 
-    handleChange = ({ fileList }) => this.setState({ fileList });
+    const handleChange = ({ fileList }) => setState({ ...state, fileList });
 
-    render() {
-        const { previewVisible, previewImage, fileList } = this.state;
-
-
-        return (
-            <div className="clearfix">
-                <Upload
-                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                    listType="picture-card"
-                    fileList={fileList}
-                    onPreview={this.handlePreview}
-                    onChange={this.handleChange}
-                >
-                </Upload>
-                <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
-                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
-                </Modal>
-            </div>
-        );
-    }
-}
+    return(
+        <div className="clearfix">
+        <Upload
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            listType="picture-card"
+            fileList={state.fileList}
+            onPreview={handlePreview}
+            onChange={handleChange}
+        >
+        </Upload>
+        <Modal visible={state.previewVisible} footer={null} onCancel={handleCancel}>
+            <img alt="example" style={{ width: '100%' }} src={state.previewImage} />
+        </Modal>
+    </div>
+    );
+};
 export default UploadFiles;
